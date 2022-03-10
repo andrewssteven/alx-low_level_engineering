7823efcdcbf09bdb5b84f27929c038a2643e507c
andrewssteven <andrewssteven151@gmail.com>
2022-03-10 12:58:48 -0800

    0-5 input 01

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main Entry point
+ *
+ *Return: Always (success)
+ */
+int main(void){
+	//string installation
+	char Mystr[] = "Programming is like building a multilingual puzzle"; 
+	puts(Mystr); //writing the string to stdout
+
+	return 0;
+	}
