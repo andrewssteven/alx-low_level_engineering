bf6c1041300305d39e954044f4e3522ff2135f2f
andrewssteven <andrewssteven151@gmail.com>
2022-03-10 13:31:23 -0800

    four and five 03

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  *Return: Always (success)
  */
 int main(void)
-	puts("\"Programming is like building a multilingual puzzle\n")
+	puts("\"Programming is like building a multilingual puzzle\n");
+
 	return (0);
 	}
