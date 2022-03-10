a32b7653a2f37f49643252f52c478a6221b78919
andrewssteven <andrewssteven151@gmail.com>
2022-03-10 13:25:53 -0800

    four and five 02

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,7 @@
  *
  *Return: Always (success)
  */
-int main(void){
-	//string installation
-	char Mystr[] = "Programming is like building a multilingual puzzle"; 
-	puts(Mystr); //writing the string to stdout
-
-	return 0;
+int main(void)
+	puts("\"Programming is like building a multilingual puzzle\n")
+	return (0);
 	}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,5 +5,6 @@
  * Return: Always 0 (Success)
  */
 int main(void){
-printf("with proper grammer, but the outcome is a piece of art")
+printf("with proper grammer, but the outcome is a piece of art\n")
 	return (0);
+}
