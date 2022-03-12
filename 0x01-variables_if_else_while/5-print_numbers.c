2d6c68e9562c51df477986e6680fcea74d5b8f10
andrewssteven <andrewssteven151@gmail.com>
2022-03-12 22:29:28 +0100

    am tired

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,8 +2,7 @@
 #include<stdlib.h>
 /**
  * main - prints all single digit numbers of base 10
- * starting from 0 , 
- * 
+ * starting from 0, 
  * followed by a new line
  * 
  * Return: Always 0 (success)
