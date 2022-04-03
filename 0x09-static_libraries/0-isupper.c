ef85229f69285b8f604350135ac4049992f53e84
andrewssteven <andrewssteven151@gmail.com>
2022-04-03 21:14:17 +0000

    try one

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - a function that checks for uppercase character.
+ * @c: the character to check
+ * Return: 1 for uppercase characters, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
