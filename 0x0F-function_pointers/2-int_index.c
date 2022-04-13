b995b4f186a3e09606da6c33d4000ff8ffc428f9
andrewssteven <andrewssteven151@gmail.com>
2022-04-13 19:31:31 +0000

    second trial

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,10 +16,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-		for (i = 0; i < size; i++)
-		{
-		if (cmp(array[i]) != 0)
-				return (i);
+	for (i = 0; i < size; i++)
+	{
+	if (cmp(array[i]) != 0)
+	return (i);
 		}
 	}
 
